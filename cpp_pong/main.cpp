62a356de413c06b83e1302ef27321824018ed201
EmmBaDev <barriem4@hotmail.com>
2024-07-17 10:05:46 +0200

    Add project files.

---
+++ cpp_pong/main.cpp
@@ -0,0 +1,16 @@
+#include "raylib.h"
+#include <iostream>
+
+int main() {
+	const int screen_width = 1200;
+	const int screen_height = 800;
+	InitWindow(screen_width, screen_height, "CPP PONG");
+
+	while (WindowShouldClose() == false)
+	{
+		
+	}
+
+	CloseWindow();
+	return 0;
+}
